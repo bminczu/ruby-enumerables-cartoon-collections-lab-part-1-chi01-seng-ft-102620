f4a2395f1bd0c860c92e6642462088870505e032
adventurous-format-6947 <bminczu@gmail.com>
2020-10-09 16:08:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def list_dwarves(array)
   hash=Hash.new
   array.each_with_index {|name,index|}
-  do array[name]= index +1
+  do hash[name]= index +1
   }
 end # Use `each_with_index` to enumerate over the provided array
   array
