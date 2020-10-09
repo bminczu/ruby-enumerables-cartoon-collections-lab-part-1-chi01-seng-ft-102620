6232fc4279bee40890e76f970ce21d95158939a2
adventurous-format-6947 <bminczu@gmail.com>
2020-10-09 15:54:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
+  array.each do |name|
+    puts "Hello #{name}"# Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
 end
